91193a1ae98ffb07e5f00485f560ee0ab0ac3565
fewerworld <fewerworld@126.com>
2020-12-24 18:36:45 +0800

    Add files via upload

---
+++ pactera/subject/weather_card/Demo/v0_7/vue.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+function resolve(dir) {
+    return path.join(__dirname, '.', dir);
+}
+
+module.exports = {
+    runtimeCompiler: true,
+    devServer:{
+      host: 'localhost',
+      port: '8800',
+      proxy: {
+          '/v7':{
+              target: 'https://devapi.qweather.com',
+              // ws: true,
+              changeOrigin: true,
+              port: '8801'
+              // ,pathRewrite: {
+              //     '^/v7':'/v7'
+              // }
+          }
+          ,'/v2':{
+              target: 'https://devapi.qweather.com',
+              changeOrigin: true,
+              port: '8802'
+              // ,pathRewrite: {
+              //     '^/v2':'/v2'
+              // }
+          }
+      }
+    },
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@':resolve('src')
+            }
+        }
+    }
+}
